f8d56e352ba2603286c095c851b0e6782d67d297
dawnkelly09 <dawnkelly09@gmail.com>
2022-12-11 12:59:46 -0800

    adds backend openai API call & .env

---
+++ pages/api/generate.js
@@ -0,0 +1,26 @@
+import { APP_PATH_ROUTES_MANIFEST } from 'next/dist/shared/lib/constants';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = ""
+const generateAction = async (req, res) => {
+    //Run first prompt
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+    const baseCompletion = await APP_PATH_ROUTES_MANIFEST.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${basePromptPrefix}${req.body.userInput}`,
+        temperature: 0.7,
+        max_tokens: 250,
+    })
+
+    const basePromptOutput = baseCompletion.data.choices.pop()
+
+    res.status(200).json({output: basePromptOutput})
+}
+
+export default generateAction
